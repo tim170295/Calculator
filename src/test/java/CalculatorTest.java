a7b40455a898f53b60a6d7d4ea17aaf6f5d0a5ce
timjurgens <tim170295@googlemail.com>
2018-11-29 15:54:08 +0100

    endlich

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    Calculator calc;
+    Calculator calc = new Calculator();
     @Test
     void justAnExample() {
        assertEquals(calc.add(5,5),10);
